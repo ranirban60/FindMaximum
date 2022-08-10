ff402ea37a28dd06690135ba487cd9bb5036f53d
ranirban60@gmail.com <ranirban60@gmail.com>
2022-08-10 09:54:55 +0530

    Added: Find maximum of three Floats

--- src/main/java/com/bridgelabz/FindTheMax.java
+++ src/main/java/com/bridgelabz/FindTheMax.java
@@ -1,5 +1,5 @@
 /*
-Given three Integer Find the maximum
+Given three Floats Find the maximum
  */
 package com.bridgelabz;
 
@@ -8,36 +8,36 @@ import java.util.Scanner;//Scanner class is used to take user input
 public class FindTheMax {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter three Integer : ");
+        System.out.println("Enter three Floats : ");
 
-        int a = sc.nextInt();//nextInt() method scans the next token of the Input as int
-        int b = sc.nextInt();//nextInt() method scans the next token of the Input as int
-        int c = sc.nextInt();//nextInt() method scans the next token of the Input as int
+        float a = sc.nextFloat();//nextInt() method scans the next token of the Input as int
+        float b = sc.nextFloat();//nextInt() method scans the next token of the Input as int
+        float c = sc.nextFloat();//nextInt() method scans the next token of the Input as int
 
-        Integer i1 = a;
-        Integer i2 = b;
-        Integer i3 = c;
+        Float i1 = a;
+        Float i2 = b;
+        Float i3 = c;
 
-        int result1 = i1.compareTo(i2);//compare between two integer by compareTo() method
+        int result1 = i1.compareTo(i2);//compare between two Float by compareTo() method
         int result2 = 0;
         int result3 = 0;
         if(result1 < 0){
-            result2 = i2.compareTo(i3);//compare between two integer by compareTo() method
+            result2 = i2.compareTo(i3);//compare between two Float by compareTo() method
         }else if(result1 > 0){
-            result3 = i1.compareTo(i3);//compare between two integer by compareTo() method
+            result3 = i1.compareTo(i3);//compare between two Float by compareTo() method
         }else{
-            result2 = i1.compareTo(i3);//compare between two integer by compareTo() method
+            result2 = i1.compareTo(i3);//compare between two Float by compareTo() method
         }
         if(result2 > 0){
-            System.out.println("Max for all three Integer is "+" "+b);
+            System.out.println("Max for all three Float is "+" "+b);
         }else if(result2 < 0){
-            System.out.println("Max for all three Integer is "+" "+c);
+            System.out.println("Max for all three Float is "+" "+c);
         }else if(result3 > 0){
-            System.out.println("Max for all three Integer is "+" "+a);
+            System.out.println("Max for all three Float is "+" "+a);
         }else if(result3 < 0){
-            System.out.println("Max for all three Integer is "+" "+c);
+            System.out.println("Max for all three Float is "+" "+c);
         }else{
-            System.out.println("Max for all three Integer is "+" "+a);
+            System.out.println("Max for all three Float is "+" "+a);
         }
     }
 }
